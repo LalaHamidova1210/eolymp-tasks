72666a5b323fda13b0bac98047bd14497619a031
LalaHamidova1210 <lalahamidova1210@gmail.com>
2024-11-07 20:36:28 +0400

    array

---
+++ src/main/java/az/turingacademy/lesson7/ArrayApp.java
@@ -0,0 +1,18 @@
+package az.turingacademy.lesson7;
+
+public class ArrayApp {
+    public static void main (String[] args) {
+
+        int [] arr = {1,6,8,10};
+        int max=arr[0];
+        int maxIndex=0;
+        for (int i =1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                maxIndex = i;
+            }
+        }
+        System.out.println("Array max value: " + max);
+        System.out.println("Max value index: " + maxIndex);
+    }
+}
